a4f17d85642c7b5bd64468bcb8927825f946dbf7
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-03-14 12:07:11 +0200

    Adding files

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()
 })
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
