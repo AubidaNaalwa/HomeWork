d45f6261c64e13514a1352a5386d1f9c6c7559b8
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-03-14 11:45:42 +0200

    Adding files

--- src/Components/AddNewWord.js
+++ src/Components/AddNewWord.js
@@ -4,11 +4,19 @@ import { observer, inject } from 'mobx-react'
 
 function AddNewWord(props) {
     const [addingWords, setWords] = useState("")
+    const addNewWordToDB = async () => {
+        try {
+            await props.dictionaryStore.addingToDictionary(addingWords)
+            setWords("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div id="container">
-            <input type="text" placeholder="Insert New Word" value={addingWords} onChange={(e)=>setWords(e.target.value)}/>
+            <input type="text" placeholder="Insert New Word" value={addingWords} onChange={(e) => setWords(e.target.value)} />
             <div id="buttons">
-                <button name="AddingNewWord" onClick={()=>props.dictionaryStore.addingToDictionary(addingWords)}>Adding </button>
+                <button name="AddingNewWord" onClick={addNewWordToDB}>Adding </button>
             </div>
         </div>
     );
