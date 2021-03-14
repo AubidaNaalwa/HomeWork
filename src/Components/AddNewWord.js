1413eb1a6aa610c6ba064962ff40a64bb79d79c0
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-03-14 11:49:19 +0200

    Adding files

--- src/Components/AddNewWord.js
+++ src/Components/AddNewWord.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-
+import ReactSnackBar from "react-js-snackbar";
 import { observer, inject } from 'mobx-react'
 
 function AddNewWord(props) {
     const [addingWords, setWords] = useState("")
+    const [show, setShow] = useState(false)
     const addNewWordToDB = async () => {
         try {
+            
             await props.dictionaryStore.addingToDictionary(addingWords)
+            setShow(true)
+            setTimeout(() => {
+                setShow(false)
+            }, 2000);
             setWords("")
         } catch (error) {
             console.log(error)
@@ -17,6 +23,9 @@ function AddNewWord(props) {
             <input type="text" placeholder="Insert New Word" value={addingWords} onChange={(e) => setWords(e.target.value)} />
             <div id="buttons">
                 <button name="AddingNewWord" onClick={addNewWordToDB}>Adding </button>
+                <ReactSnackBar Icon={<span>🦄</span>} Show={show}>
+                    Word Saved
+                </ReactSnackBar>
             </div>
         </div>
     );
